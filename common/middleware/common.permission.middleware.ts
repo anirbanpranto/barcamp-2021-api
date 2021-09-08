f9274d2ac37d53a924fc74bf79a421cb0daf05a6
Anirban Bala <anirbanpranto@gmail.com>
2021-09-09 01:40:05 +0800

    fix : removed console log

--- common/middleware/common.permission.middleware.ts
+++ common/middleware/common.permission.middleware.ts
@@ -15,9 +15,9 @@ class CommonPermissionMiddleware{
                 const userPermissionFlags = parseInt(
                     res.locals.jwt.permissionFlags
                 );
-                console.log(res.locals.jwt);
-                console.log(userPermissionFlags)
-                console.log(requiredPermissionFlag)
+                // console.log(res.locals.jwt);
+                // console.log(userPermissionFlags)
+                // console.log(requiredPermissionFlag)
                 if (userPermissionFlags & requiredPermissionFlag) {
                     next();
                 } else {
