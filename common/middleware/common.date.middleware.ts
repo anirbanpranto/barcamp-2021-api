6300a7e8e523df83fccfe736c848409f12174c1a
Shaun Mak <60981304+Shaunmak1214@users.noreply.github.com>
2021-09-14 07:36:33 +0800

    fix: date middleware

--- common/middleware/common.date.middleware.ts
+++ common/middleware/common.date.middleware.ts
@@ -11,16 +11,15 @@ class CommonDateMiddleware {
   
   private proposeTopicDateRange = [new Date("15 September, 2020 00:00:00"), new Date("24 September, 2020 00:00:00")];
   private voteTopicDateRange = [];
-  private dateNow = this.convertTZ(`${new Date()}`, 'Asia/Kuala_Lumpur');
 
   convertTZ(date: string, tzString: string) {
     return new Date((typeof date === "string" ? new Date(date) : date).toLocaleString("en-US", {timeZone: tzString}));   
   }
 
   validateDateRange(routesType: string) {
+    const dateNow = this.convertTZ(`${new Date()}`, 'Asia/Kuala_Lumpur');
     return (req: express.Request, res: express.Response, next: express.NextFunction) => {
       const dateRange = routesType === 'proposeTopic' ? this.proposeTopicDateRange : this.voteTopicDateRange;
-      const dateNow = this.dateNow;
       const dateRangeStart = dateRange[0];
       const dateRangeEnd = dateRange[1];
       const dateRangeStartString = dateRangeStart.toString();
